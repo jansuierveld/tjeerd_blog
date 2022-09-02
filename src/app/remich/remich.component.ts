c7e6029301b7be2b03c8640470c17201121e5fae
Tjeerd Suierveld <tjeerdsuierveld3@gmail.com>
2022-09-02 15:25:26 +0200

    Remich update 1

--- src/app/remich/remich.component.ts
+++ src/app/remich/remich.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { IntroService } from '../intro.service';
 
 @Component({
   selector: 'app-remich',
   templateUrl: './remich.component.html',
   styleUrls: ['./remich.component.scss']
 })
-export class RemichComponent implements OnInit {
+export class RemichComponent  implements OnInit {
 
-  constructor() { }
+  constructor(readonly introService: IntroService) { }
 
   ngOnInit(): void {
+    this.introService.intro$.next(false);
   }
 
 }
