906354f6d1553cfa6f3e217b5bff856dfe6cd84e
Tjeerd Suierveld <tjeerdsuierveld3@gmail.com>
2022-09-02 20:37:35 +0200

    Nancy update 1

--- src/app/nancy/nancy.component.ts
+++ src/app/nancy/nancy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IntroService } from '../intro.service';
 
 @Component({
   selector: 'app-nancy',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NancyComponent implements OnInit {
 
-  constructor() { }
+  constructor(readonly introService: IntroService) { }
 
   ngOnInit(): void {
+    this.introService.intro$.next(false);
   }
 
 }
