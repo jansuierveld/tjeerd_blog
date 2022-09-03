fe34011738a1dcea40fee0554412b9abc581ff30
jansuierveld <jansuierveld@gmail.com>
2022-09-03 09:44:44 +0200

    comp

---
+++ src/app/manosque/manosque.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manosque',
+  templateUrl: './manosque.component.html',
+  styleUrls: ['./manosque.component.scss']
+})
+export class ManosqueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
