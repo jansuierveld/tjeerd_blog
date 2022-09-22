71b4f7edcb691e6119a265f1cb2c780f63ac5864
jansuierveld <jansuierveld@gmail.com>
2022-09-22 18:49:54 +0200

    banner

--- src/app/stvith/stvith.component.ts
+++ src/app/stvith/stvith.component.ts
@@ -16,9 +16,9 @@ export class StvithComponent implements OnInit {
   constructor(readonly introService: IntroService) {
     this.banner = new Banner(
       'ca-pub-8239777228243047',
-      5201642322,
+      '5702571326' ,
       'auto',
-      true
+      'true'
     )
    }
 
